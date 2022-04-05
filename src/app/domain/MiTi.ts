eb4f8d422edb6b0333d2a4aa43d58d1e8a5bf7f3
charlotte.russell <charlotte.russell@openknowledge.de>
2022-04-05 17:07:09 +0200

    add functionality to viewlunch component, create miti object

---
+++ src/app/domain/MiTi.ts
@@ -0,0 +1,8 @@
+export type MiTi = {
+  locality: string,
+  location: string,
+  firstname: string,
+  lastname: string,
+  hour: number,
+  minute: number,
+}
--- src/app/viewlunch/viewlunch.component.ts
+++ src/app/viewlunch/viewlunch.component.ts
@@ -1,13 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import {MiTi} from "../domain/MiTi";
+
+const mitiObject = {
+  locality: 'Schlöfe',
+  location: 'Oldenburg',
+  firstname: 'test1',
+  lastname: 'test2',
+  hour: 1,
+  minute: 2,
+}
 
 @Component({
   selector: 'app-viewlunch',
   templateUrl: './viewlunch.component.html',
   styleUrls: ['./viewlunch.component.css']
 })
+
 export class ViewlunchComponent implements OnInit {
 
-  constructor() { }
+  mities: MiTi[] = [];
+
+  constructor() {
+    this.mities = [
+      {locality: 'Schlöfe',
+        location: 'Oldenburg',
+        firstname: 'test1',
+        lastname: 'test2',
+        hour: 1,
+        minute: 2,}
+    ]
+  }
 
   ngOnInit(): void {
   }
