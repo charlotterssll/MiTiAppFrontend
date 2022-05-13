8f19ce16cc95494dd2d18f3b8c78b17cd3f670a5
charlotte.russell <charlotte.russell@openknowledge.de>
2022-05-13 14:14:04 +0200

    implement request tests

--- src/app/service/request.spec.ts
+++ src/app/service/request.spec.ts
@@ -1,52 +1,114 @@
 import { Miti } from '../domain/miti/Miti';
-import { rest } from 'msw';
-import { setupServer } from 'msw/node';
-import { render } from '@testing-library/angular';
-import { ViewComponent } from '../view/view.component';
-import { AppComponent } from '../app.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { MitiService } from './miti.service';
-import { of } from 'rxjs';
-import { createMock } from '@testing-library/angular/jest-utils';
-import { APP_BASE_HREF } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
 
-describe('Miti API Test', () => {
-  test('should post miti object to api', async () => {
+describe('Miti Request Test', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let mitiService: MitiService;
+  let mitiId = '1';
+  let dummyMiti: Miti = {
+    place: {
+      locality: {
+        value: 'Schloefe',
+      },
+      location: {
+        value: 'Oldenburg',
+      },
+    },
+    employee: {
+      firstName: {
+        value: 'Charlotte',
+      },
+      lastName: {
+        value: 'Russell',
+      },
+    },
+    time: {
+      value: '12:00',
+    },
+    mitiId: '1',
+  };
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [MitiService],
+    });
+    httpClient = TestBed.inject(HttpClient);
+    mitiService = TestBed.inject(MitiService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  test('perform a get request and should return null', () => {
+    mitiService.readMiti().subscribe((response: Miti[]) => {
+      [dummyMiti] = response;
+    });
+    const req = httpTestingController.expectOne('http://localhost:8080/miti');
+    expect(req.request.method).toBe('GET');
+    expect(req.request.body).toBe(null);
+  });
+
+  test('perform a get request by id and should return null', async () => {
+    mitiService.readMitiByMitiId(mitiId).subscribe();
+    const req = httpTestingController.expectOne('http://localhost:8080/miti/1');
+    expect(req.request.method).toEqual('GET');
+    expect(req.request.body).toEqual(null);
+  });
+
+  test('should return a post request', async () => {
+    mitiService.createMiti(dummyMiti).subscribe();
+    const req = httpTestingController.expectOne('http://localhost:8080/miti');
+    expect(req.request.method).toEqual('POST');
+    expect(req.request.body).toEqual(dummyMiti);
+  });
+
+  test('should return a put request', async () => {
+    mitiService.updateMiti(mitiId, dummyMiti).subscribe();
+    const req = httpTestingController.expectOne('http://localhost:8080/miti/1');
+    expect(req.request.method).toEqual('PUT');
+    expect(req.request.body).toEqual(dummyMiti);
+  });
+
+  test('should return a delete request', async () => {
+    mitiService.deleteMiti(mitiId).subscribe();
+    const req = httpTestingController.expectOne('http://localhost:8080/miti/1');
+    expect(req.request.method).toEqual('DELETE');
+    expect(req.request.body).toEqual(null);
+  });
+
+  /*test('should get one miti', async () => {
     await render(ViewComponent, {
-      providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
-      declarations: [AppComponent, ViewComponent],
-      imports: [BrowserModule, FormsModule, HttpClientModule],
+      declarations: [AppComponent, ViewComponent, UpdateComponent],
+      imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        HttpClientTestingModule,
+        RouterModule,
+        AppRoutingModule,
+      ],
+      componentProviders: [
+        {
+          provide: MitiService,
+        },
+      ],
     });
 
-    const testMiti: Miti[] = [
-      {
-        mitiId: '1',
-        place: {
-          locality: {
-            value: 'Schloefe',
-          },
-          location: {
-            value: 'Oldenburg',
-          },
-        },
-        employee: {
-          firstName: {
-            value: 'Charlotte',
-          },
-          lastName: {
-            value: 'Russell',
-          },
-        },
-        time: {
-          value: '12:00',
-        },
-      },
-    ];
+    const mitiItems = await screen.getAllByRole('rowgroup');
+    expect(mitiItems).toHaveLength(dummyMiti.length);
 
     const mitiService = createMock(MitiService);
-    mitiService.readMiti = jest.fn(() => of(testMiti));
+    mitiService.readMiti = jest.fn(() => of(mitis));
 
     const mitiServer = setupServer(
       rest.get('http://localhost:8080/miti', (req, res, ctx) => {
@@ -56,5 +118,5 @@ describe('Miti API Test', () => {
         return res(ctx.status(200), ctx.json(testMiti));
       })
     );
-  });
+  });*/
 });
