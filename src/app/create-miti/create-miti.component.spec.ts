bebb2b41ae27a7fb45608f9ec402795e38062d47
charlotte.russell <charlotte.russell@openknowledge.de>
2022-05-24 14:18:50 +0200

    refactor of createmiti test with testUtilityFunction

--- src/app/create-miti/create-miti.component.spec.ts
+++ src/app/create-miti/create-miti.component.spec.ts
@@ -1,13 +1,20 @@
 import { setupServer } from 'msw/node';
-import { rest } from 'msw';
-import { getByText, render, screen } from '@testing-library/angular';
+import { MockedRequest, rest } from 'msw';
+import { render, RenderResult, screen } from '@testing-library/angular';
 import { ReadMitiComponent } from '../read-miti/read-miti.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { Miti } from '../domain/miti/Miti';
 import { CreateMitiComponent } from './create-miti.component';
+import { AppComponent } from '../app.component';
+import { UpdateMitiComponent } from '../update-miti/update-miti.component';
+import { DeleteMitiComponent } from '../delete-miti/delete-miti.component';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from '../app-routing.module';
 
 describe('Employee wants to create...', () => {
+  let rendered: RenderResult<AppComponent>;
+
   const server = setupServer(
     rest.post('http://localhost:8080/miti', (req, res, ctx) => {
       let dummyMiti: Miti = {
@@ -37,7 +44,6 @@ describe('Employee wants to create...', () => {
       };
       return res(ctx.status(200), ctx.json(dummyMiti));
     }),
-
     rest.get('http://localhost:8080/miti', (req, res, ctx) => {
       return res(
         ctx.json([
@@ -71,20 +77,45 @@ describe('Employee wants to create...', () => {
     })
   );
 
+  const testUtilityFunction = new Promise<void>(async (resolve) => {
+    const listener = async (request: MockedRequest) => {
+      if (request.url.href === 'http://localhost:8080/miti') {
+        setTimeout(resolve, 0);
+        server.events.removeListener('request:end', listener);
+      }
+    };
+    server.events.on('request:end', listener);
+  });
+
+  beforeEach(async () => {
+    rendered = await render(AppComponent, {
+      declarations: [
+        CreateMitiComponent,
+        ReadMitiComponent,
+        UpdateMitiComponent,
+        DeleteMitiComponent,
+      ],
+      imports: [FormsModule, HttpClientModule, RouterModule, AppRoutingModule],
+      routes: [
+        { path: 'update/:id', component: UpdateMitiComponent },
+        { path: '', component: ReadMitiComponent, pathMatch: 'full' },
+      ],
+    });
+  });
   beforeAll(() => server.listen());
   afterEach(() => server.resetHandlers());
   afterAll(() => server.close());
 
   test('...a lunch table', async () => {
-    const rendered = await render(ReadMitiComponent, {
-      declarations: [ReadMitiComponent, CreateMitiComponent],
-      imports: [FormsModule, HttpClientModule],
-    });
-    await rendered.fixture.detectChanges();
-    await new Promise((resolve) => {
-      setTimeout(resolve, 10);
-    });
-    await rendered.fixture.detectChanges();
+    await rendered.fixture.detectChanges(); // ensure ngOnInit is executed
+    await testUtilityFunction;
+    await rendered.fixture.detectChanges(); // ensure template is rendered after request
+
+    expect(screen.getByText('Immergrün')).toBeInTheDocument();
+    expect(screen.getByText('Oldenburg')).toBeInTheDocument();
     expect(screen.getByText('Hannelore')).toBeInTheDocument();
+    expect(screen.getByText('Kranz')).toBeInTheDocument();
+    expect(screen.getByText('12:00')).toBeInTheDocument();
+    expect(screen.getByText('2022-04-01')).toBeInTheDocument();
   });
 });
