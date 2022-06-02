489641320bae7fcc3c2ca5902231c94c1d12bccf
okMari <marian.heck@openknowledge.de>
2022-06-02 14:56:33 +0200

    implementaion of regex alerts

--- src/app/create-miti/create-miti.component.ts
+++ src/app/create-miti/create-miti.component.ts
@@ -15,6 +15,13 @@ export class CreateMitiComponent {
   time?: string;
   date?: string;
   alertNull?: string;
+  alertLocality?: string;
+  alertLocation?: string;
+  alertFirstName?: string;
+  alertLastName?: string;
+  alertTime?: string;
+  alertDate?: string;
+  alertMitiAlreadyExists?: string;
 
   constructor(private mitiService: MitiService, private router: Router) {}
 
@@ -30,10 +37,57 @@ export class CreateMitiComponent {
       this.alertNull = 'Null values in any form fields are disallowed';
       console.log('Null values in any form fields are disallowed');
     } else {
-      this.createMiti();
+      this.youShallMeetRegexPattern();
     }
   }
 
+  youShallMeetRegexPattern() {
+    const placeEmployeePattern = new RegExp(
+      '[A-ZÄÖU][a-zäöüß-]+(\\s[A-ZÄÖÜ][a-zäöüß-]+)*'
+    );
+    const timePattern = new RegExp('^(2[0-3]|[01]?[0-9]):([0-5]?[0-9])$');
+    const datePattern = new RegExp(
+      '^\\s*((?:19|20)\\d{2})\\-(1[012]|0?[1-9])\\-(3[01]|[12][0-9]|0?[1-9])\\s*$'
+    );
+    if (!placeEmployeePattern.test(<string>this.locality)) {
+      this.alertLocality =
+        'Locality must only contain letters and begin with upper case';
+      console.log(
+        'Locality must only contain letters and begin with upper case'
+      );
+    }
+    if (!placeEmployeePattern.test(<string>this.location)) {
+      this.alertLocation =
+        'Location must only contain letters and begin with upper case';
+      console.log(
+        'Location must only contain letters and begin with upper case'
+      );
+    }
+    if (!placeEmployeePattern.test(<string>this.firstName)) {
+      this.alertFirstName =
+        'FirstName must only contain letters and begin with upper case';
+      console.log(
+        'FirstName must only contain letters and begin with upper case'
+      );
+    }
+    if (!placeEmployeePattern.test(<string>this.lastName)) {
+      this.alertLastName =
+        'LastName must only contain letters and begin with upper case';
+      console.log(
+        'LastName must only contain letters and begin with upper case'
+      );
+    }
+    if (!timePattern.test(<string>this.time)) {
+      this.alertTime = 'Time must only contain numbers in 24h time format';
+      console.log('Time must only contain numbers in 24h time format');
+    }
+    if (!datePattern.test(<string>this.date)) {
+      this.alertDate = 'Date must only contain numbers YYYY-MM-DD format';
+      console.log('Date must only contain numbers YYYY-MM-DD format');
+    } else {
+      this.createMiti();
+    }
+  }
   createMiti() {
     const mitiJson = {
       place: {
@@ -47,12 +101,26 @@ export class CreateMitiComponent {
       time: this.time,
       date: this.date,
     };
-    console.log('POST Miti: ', mitiJson);
-    return this.mitiService.createMiti(mitiJson).subscribe(() => {
-      let currentUrl = this.router.url;
-      this.router.routeReuseStrategy.shouldReuseRoute = () => false;
-      this.router.onSameUrlNavigation = 'reload';
-      this.router.navigate([currentUrl]);
-    });
+
+    return this.mitiService.createMiti(mitiJson).subscribe(
+      () => {
+        let currentUrl = this.router.url;
+        this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+        this.router.onSameUrlNavigation = 'reload';
+        this.router.navigate([currentUrl]);
+        console.log('POST Miti: ', mitiJson);
+      },
+      (error) => {
+        if (
+          error.error.message ===
+          'Employee already has a lunch table meeting on this day!'
+        ) {
+          this.alertMitiAlreadyExists = error.error.message;
+          console.log(error.error.message);
+        } else {
+          console.log(error.error.message);
+        }
+      }
+    );
   }
 }
